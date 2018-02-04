566d3fc0e393a8a14baf4bc0564ab0cd2855148e
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-02-03 23:18:44 -0500

    Add Hiccup library to project dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
-                 [compojure "1.6.0"]]
+                 [compojure "1.6.0"]
+                 [hiccup "1.0.5"]]
   :main randomgif.core
   :profiles {:dev
              {:main randomgif.core/-dev-main}})
