9d22f7ef14661831cde0818c51acc30c5cd80e0f
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-02-03 23:02:29 -0500

    Add Ring middleware for reload of app in dev

--- src/randomgif/core.clj
+++ src/randomgif/core.clj
@@ -1,5 +1,6 @@
 (ns randomgif.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request]
   "Function to handle basic HTTP request"
@@ -11,4 +12,10 @@
   "The entry point for the RandomGIF App"
   []
   (jetty/run-jetty handler
+    {:port 8000}))
+
+(defn -dev-main
+  "The dev entry point for the RandomGIF App with app reload functionality"
+  []
+  (jetty/run-jetty (wrap-reload #'handler)
     {:port 8000}))
