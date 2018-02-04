e1d3a9c5a4be1370dfb6e41e80bab27d3943ce75
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-02-03 23:09:33 -0500

    Add basic route set up with Compojure for "/" and not found

--- src/randomgif/core.clj
+++ src/randomgif/core.clj
@@ -1,6 +1,8 @@
 (ns randomgif.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
 (defn handler [request]
   "Function to handle basic HTTP request"
@@ -8,14 +10,18 @@
    :body "Welcome to the RandomGIF App!"
    :headers {}})
 
+(defroutes app
+  (GET "/" [] handler)
+  (route/not-found "<h1>Page Not Found</h1>"))
+
 (defn -main
   "The entry point for the RandomGIF App"
   []
-  (jetty/run-jetty handler
+  (jetty/run-jetty app
     {:port 8000}))
 
 (defn -dev-main
   "The dev entry point for the RandomGIF App with app reload functionality"
   []
-  (jetty/run-jetty (wrap-reload #'handler)
+  (jetty/run-jetty (wrap-reload #'app)
     {:port 8000}))
