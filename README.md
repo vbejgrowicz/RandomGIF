d160d8b56dc86035fe623693c8618114fe674995
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-02-03 22:43:45 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RandomGIF
+
+A Clojure library designed to fetch random GIFs.
+
+## License
+
+Copyright © 2018 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject randomgif "0.1.0-SNAPSHOT"
+  :description "RandomGIF: A Clojure library designed to fetch random GIFs."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
