5b21f7dd75b59dd7dec06937b0f2eb173df71252
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-02-03 22:55:59 -0500

    Add main for app entry and route handler to handle basic route request

--- src/randomgif/core.clj
+++ src/randomgif/core.clj
@@ -1,6 +1,14 @@
-(ns randomgif.core)
+(ns randomgif.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [request]
+  "Function to handle basic HTTP request"
+  {:status 200
+   :body "Welcome to the RandomGIF App!"
+   :headers {}})
+
+(defn -main
+  "The entry point for the RandomGIF App"
+  []
+  (jetty/run-jetty handler
+    {:port 8000}))
